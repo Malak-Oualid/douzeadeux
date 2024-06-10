8f989f726f0ea4135bdcb3eed0edd7dad58233d9
Malak-Oualid <144572770+Malak-Oualid@users.noreply.github.com>
2024-06-10 14:27:39 -0400

    deployement error

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,6 +3,19 @@ import tailwindcss from 'tailwindcss';
 
 export default {
   plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
     tailwindcss,
     autoprefixer,
   ],
