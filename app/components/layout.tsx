3e86435f61b30167ca34e67f86be8e9ec76bdfb3
Malak-Oualid <144572770+Malak-Oualid@users.noreply.github.com>
2024-06-10 08:53:26 -0400

    first functionalities

---
+++ app/components/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "./navbar";
+import Footer from "./footer";
+import React from 'react';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className="content">
+            <Navbar />
+            {children}
+            <Footer />
+        </div>
+      );
+}
+ 
+export default Layout;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,tsx}", // Adjust the path if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
